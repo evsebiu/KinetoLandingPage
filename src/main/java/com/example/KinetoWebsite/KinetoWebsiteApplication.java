2f9ef809f24872f38123ce5a68a1d648e60e67c1
evsebiu <evsebiu@gmail.com>
2025-10-30 20:13:48 +0200

    fifth commit

--- src/main/java/com/example/KinetoWebsite/KinetoWebsiteApplication.java
+++ src/main/java/com/example/KinetoWebsite/KinetoWebsiteApplication.java
@@ -1,7 +1,17 @@
 package com.example.KinetoWebsite;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Community Health Medical System API",
+                version = "1.0",
+                description = "API for Community Health Medical System"
+        )
+)
 
 @SpringBootApplication
 public class KinetoWebsiteApplication {
